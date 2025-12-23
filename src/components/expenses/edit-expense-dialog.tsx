1539432f51e33528cd675cbd452ccf8c8b384ee1
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-23 19:36:53 +0000

    Act as a senior frontend engineer specialized in React, Next.js (App Rou

--- src/components/expenses/edit-expense-dialog.tsx
+++ src/components/expenses/edit-expense-dialog.tsx
@@ -140,5 +140,3 @@ export default function EditExpenseDialog({ expense, isOpen, onClose, onExpenseU
     </Dialog>
   );
 }
-
-    
--- src/components/goals/edit-goal-dialog.tsx
+++ src/components/goals/edit-goal-dialog.tsx
@@ -137,5 +137,3 @@ export default function EditGoalDialog({ goal, isOpen, onClose, onGoalUpdated }:
     </Dialog>
   );
 }
-
-    
