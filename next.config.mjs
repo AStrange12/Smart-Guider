e6480d8db18239aabaf956e145a639a86e83c601
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-26 18:08:48 +0000

    add this for AI-Powered Financial Advice --- https://ruahtech.com.au/wp-

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.infinitysolutions.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.prod.website-files.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'fonsecaadvisers.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.infinitysolutions.com',
+            },
+             {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.prod.website-files.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'fonsecaadvisers.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ruahtech.com.au'
+            }
+        ],
+    },
 };
 
 export default nextConfig;
