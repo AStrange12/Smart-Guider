31a36a06e217a5efc3a14ce817e91df37aa03087
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-26 18:15:57 +0000

    add this image for Savings Goal Tracking -- https://www.treasury.id/wp-c

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,42 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.ibb.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.infinitysolutions.com',
-            },
-             {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.prod.website-files.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'fonsecaadvisers.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'ruahtech.com.au'
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.infinitysolutions.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.prod.website-files.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ruahtech.com.au',
+      },
+      {
+        protocol: 'https”',
+        hostname: 'fonsecaadvisers.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.treasury.id',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
