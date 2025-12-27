c63668e119b6f14eaff46cd09610825ee450c5d2
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-27 08:11:22 +0000

    Act as a senior full-stack engineer and fix the deployment and build fai

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,12 +55,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
       }}
       {...props}
     />
@@ -69,6 +65,3 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
-
-
-
